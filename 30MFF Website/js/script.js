9251f4643129770da095778785424db40b800470
l-mccarthy <98512628+l-mccarthy@users.noreply.github.com>
2022-09-21 10:58:54 +0400

    Update script.js

--- 30MFF Website/js/script.js
+++ 30MFF Website/js/script.js
@@ -60,7 +60,6 @@ $(document).ready(function () {
     // Add smooth scrolling to all links
     $("a").on('click', function (event) {
 
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
             // Prevent default anchor click behavior
             event.preventDefault();
@@ -69,7 +68,7 @@ $(document).ready(function () {
             var hash = this.hash;
 
             // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            // 800 specifies no. of milliseconds it takes to scroll to specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function () {
